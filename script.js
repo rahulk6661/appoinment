cd91365158dca94149bbf63d942fd93df0f19071
rahulk6661 <133742767+rahulk6661@users.noreply.github.com>
2023-07-17 22:26:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 function addItem(event){
     event.preventDefault();
-   var username=document.getElementById('name').value;
+   let username=document.getElementById('name').value;
     var email=document.getElementById('email').value;
    var phone_number=document.getElementById('phone_number').value;
     
@@ -18,14 +18,20 @@ function addItem(event){
   deleteBtn.className='delete';
    deleteBtn.className='btn btn-sm float-right btn-danger delete';
  deleteBtn.appendChild(document.createTextNode('Delete'));
+//creating edit Button
+   var editBtn=document.createElement('button');
+  editBtn.className='edit';
+   editBtn.className='btn btn-sm float-right btn-danger edit';
+ editBtn.appendChild(document.createTextNode('edit'));
 
 
     item.appendChild(document.createTextNode(username+" "+email+" "+phone_number+" "));
    item.appendChild(deleteBtn);
+   item.appendChild(editBtn);
    var obj={
        "username":username,
        "email":email,
-       "phone number":phone_number
+       "phone":phone_number
   };
     
     
@@ -33,11 +39,13 @@ function addItem(event){
 
   localStorage.setItem(JSON.parse(JSON.stringify(obj.username)), JSON.stringify(obj));
   localStorage.setItem(username,JSON.stringify(obj));
-    
-    
+  document.getElementById('name').value="";
+  document.getElementById('email').value=""; 
+  document.getElementById('phone_number').value="";
 }
  userList.addEventListener('click',deleteItem);
-
+ userList.addEventListener('click',edititem);
+ 
  function deleteItem(e){
     if(e.target.classList.contains('delete')){
        var li = e.target.parentElement;       
@@ -45,4 +53,32 @@ function addItem(event){
         console.log(Object.keys(e));
         localStorage.removeItem("isTrusted");
     }
+    
+    
  }
+ function edititem(e)
+ {
+  if(e.target.classList.contains('edit'))
+  {
+     var li=e.target;
+     let data=li.parentNode.textContent;
+     let slice=data.split(" ");
+     let data2=slice[0];
+     
+     const user=JSON.parse(localStorage.getItem(data2));
+     console.log(user);
+    const username=user.username;
+     const email=user.email;
+     const phone=user.phone;
+     localStorage.removeItem(user);
+     
+     document.getElementById('name').value=username;
+     document.getElementById('email').value=email;
+     document.getElementById('phone_number').value=phone;
+     var li = e.target.parentElement;       
+     userList.removeChild(li);
+    
+  }
+
+
+  }
